bca75ca6d5d0f7686f3561f7f86c551637829b0e
Timur Galkin <283056@student.pwr.edu.pl>
2024-06-14 15:17:14 +0200

    Customer class update

--- src/main/java/com/example/carmanager/Customer.java
+++ src/main/java/com/example/carmanager/Customer.java
@@ -1,7 +1,60 @@
 package com.example.carmanager;
 
+import java.util.List;
+
 public class Customer {
     private String name;
     private String surname;
     private String ID;
+    private String phone;
+    private String mail;
+    private List<Vehicle> bookedVehicles;
+
+    public Customer(String name, String surname, String ID, String phone, String mail) {
+        this.name = name;
+        this.surname = surname;
+        this.ID = ID;
+        this.phone = phone;
+        this.mail = mail;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
 }
